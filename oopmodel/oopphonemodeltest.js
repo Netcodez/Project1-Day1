f0556ab555047a3d6501afeadf42d6d60a839ff4
Neto Onyiaji <onyiajineto@gmail.com>
2017-03-13 19:05:08 +0100

    phone model test

--- oopmodel/oopphonemodeltest.js
+++ oopmodel/oopphonemodeltest.js
@@ -1,41 +1,46 @@
 (function(){
   'use strict';
-var Phone = require('./oopphonemodel.js').Phone;
 
-  describe("Phone Class: Create a phone, make it work(power it on, switch it off, charge it, if full stop charging)", function() {
+ 
+
+var Phone = require('./phonemodel.JS').Phone;
+
+  describe("Phone Class: Create a phone, make it work", function() {
 
     it("The phone should be a type of `object`, and an instance of the `Phone` class", function() {
-      var apple = new Phone('Apple');
-      expect(typeof apple).toEqual(typeof {});
-      expect(apple instanceof Phone).toBeTruthy();
+      var infinix = new Phone('Infinix');
+      expect(typeof infinix).toEqual(typeof {});
+      expect(infinix instanceof Phone).toBeTruthy();
     });
 
     it("The phone should be called 'General' if no name is passed as a parameter", function() {
       var general = new Phone();
       expect(general.name).toEqual('General');
-      expect(general.model).toBe('General model');
+      expect(general.model).toBe('Gen model');
     });
 
     it("The phone name and model should be a property of the phone", function() {
-      var itel  = new Phone('Tecno', 'C7');
-      expect(itel.name).toBe('Tecno');
+      var tecno  = new Phone('Tecno', 'C7');
+      expect(tecno.name).toBe('Tecno');
       expect(tecno.model).toBe('C7');
     });
 
-    it("The phone should not be a double sim phone except its a Gionee or Tecno", function() {
-      var iPhone  = new Phone('iPhone', 'iPhone 6S', "iOS");
+    it("The phone should not be a double sim phone except its a Infinix or Tecno", function() {
+      var iPhone  = new Phone('iPhone', 'iPhone 6s', "iOS");
       expect(iPhone.doubleSim).not.toBe(true);
 
-      var tecno = new Phone('Tecno', 'C9');
+
+      var tecno = new Phone('Tecno', 'C7');
       expect(tecno.doubleSim).toBe(true);
-      expect(tecno.isCharging).toBe(false)
+      expect(tecno.isCharging).toBe(false);
+    
       tecno.charge();
       expect(tecno.isCharging).toBeTruthy();
-      expect((function(){return new Phone('Gionee', 'S9');}()).doubleSim).not.toBe(false);
-    });
+      expect((function(){return new Phone('Gionee', 'M2');}()).doubleSim).not.toBe(false);
+      });
 
     it("The phone should be powered off by default until powered on", function() {
-      var iPhone  = new Phone('iPhone', 'iPhone 6S', "iOS");
+      var iPhone  = new Phone('iPhone', 'iPhone 6s', "iOS");
       expect(iPhone.isPowerOn).toBe(false);
       expect(iPhone.isAndroid).not.toBeTruthy();
       expect(iPhone.isIphone).toBeTruthy();
@@ -46,11 +51,11 @@ var Phone = require('./oopphonemodel.js').Phone;
     });
 
     it("The Phone should charge when plugged in with the function 'charge'", function() {
-      var samsunggalaxy = new Phone("Samsung","Galaxy S8");
-      expect(samsunggalaxy.isCharging).not.toBeTruthy();
-      samsunggalaxy.charge();
-      expect(samsunggalaxy.isCharging).toBe(true);
+      var blackberry = new Phone("Blackberry","Z10");
+      expect(blackberry.isCharging).not.toBeTruthy();
+      blackberry.charge();
+      expect(blackberry.isCharging).toBe(true);
     });
 
   });
-});
+})();
